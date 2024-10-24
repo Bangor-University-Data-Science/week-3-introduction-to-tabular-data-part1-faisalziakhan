4b5866dfb45e971736e3561c7642fec03423d61d
faisalziakhan <faisalziakhan1991@gmail.com>
2024-10-24 12:02:21 +0100

    lab3 updated

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 def load_titanic_data(filepath: str) -> pd.DataFrame:
     try:
-        df = pd.read_csv("C:/Users/Laptop Land/Desktop/data science assignment 3/week-3-introduction-to-tabular-data-part1-faisalziakhan/data/titanic.csv")
+     
+        df = pd.read_csv(filepath)
         return df
     except FileNotFoundError:
         print(f"Error: The file at {filepath} was not found.")
@@ -13,4 +14,3 @@ def load_titanic_data(filepath: str) -> pd.DataFrame:
     except Exception as e:
         print(f"An error occurred: {e}")
         return pd.DataFrame()  
-
