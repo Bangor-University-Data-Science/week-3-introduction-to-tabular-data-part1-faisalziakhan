868bc75d007d646775d82a4ef6e9a43718eb9da3
faisalziakhan <faisalziakhan1991@gmail.com>
2024-10-24 12:07:01 +0100

    updated lab3

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 def load_titanic_data(filepath: str) -> pd.DataFrame:
+ 
     try:
-     
         df = pd.read_csv(filepath)
         return df
     except FileNotFoundError:
-        print(f"Error: The file at {filepath} was not found.")
-        return pd.DataFrame()  
+        print(f"File not found: {filepath}")
+        raise
     except pd.errors.EmptyDataError:
-        print("Error: The file is empty.")
-        return pd.DataFrame()  
+        print(f"The file {filepath} is empty.")
+        raise
     except Exception as e:
         print(f"An error occurred: {e}")
-        return pd.DataFrame()  
+        raise
